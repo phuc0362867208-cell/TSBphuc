49eb5c8724f2ef4cfe58226f24ec43856c7d214f
phuc0362867208-cell <phuc0362867208@gmail.com>
2026-03-11 22:35:46 +0700

    Update camlock.lua

--- camlock.lua
+++ camlock.lua
@@ -1,4 +1,4 @@
--- CAMLOCK MODULE (BLUE UI VERSION)
+-- CAMLOCK MODULE (BLUE UI + MOBILE DRAG)
 
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
@@ -94,6 +94,7 @@ end)
 
 local ScreenGui = Instance.new("ScreenGui")
 ScreenGui.Name = "CamlockUI"
+ScreenGui.ResetOnSpawn = false
 ScreenGui.Parent = lp:WaitForChild("PlayerGui")
 
 
@@ -163,26 +164,43 @@ end)
 
 
 
--- DRAG UI
+-- DRAG UI (PC + MOBILE)
 
 local dragging = false
 local dragInput
 local dragStart
 local startPos
 
+local function update(input)
+
+    local delta = input.Position - dragStart
+
+    Frame.Position =
+        UDim2.new(
+            startPos.X.Scale,
+            startPos.X.Offset + delta.X,
+            startPos.Y.Scale,
+            startPos.Y.Offset + delta.Y
+        )
+
+end
+
 
 Frame.InputBegan:Connect(function(input)
 
-    if input.UserInputType == Enum.UserInputType.MouseButton1 then
+    if input.UserInputType == Enum.UserInputType.MouseButton1
+    or input.UserInputType == Enum.UserInputType.Touch then
 
         dragging = true
         dragStart = input.Position
         startPos = Frame.Position
 
         input.Changed:Connect(function()
+
             if input.UserInputState == Enum.UserInputState.End then
                 dragging = false
             end
+
         end)
 
     end
@@ -192,8 +210,11 @@ end)
 
 Frame.InputChanged:Connect(function(input)
 
-    if input.UserInputType == Enum.UserInputType.MouseMovement then
+    if input.UserInputType == Enum.UserInputType.MouseMovement
+    or input.UserInputType == Enum.UserInputType.Touch then
+
         dragInput = input
+
     end
 
 end)
@@ -202,17 +223,7 @@ end)
 UIS.InputChanged:Connect(function(input)
 
     if input == dragInput and dragging then
-
-        local delta = input.Position - dragStart
-
-        Frame.Position =
-        UDim2.new(
-            startPos.X.Scale,
-            startPos.X.Offset + delta.X,
-            startPos.Y.Scale,
-            startPos.Y.Offset + delta.Y
-        )
-
+        update(input)
     end
 
 end)
